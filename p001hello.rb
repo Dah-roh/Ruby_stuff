975423c1036c6293b61f56ae43bd77a33509763a
Daro <mac@Daro-2.local>
2020-08-26 00:07:10 +0100

    add new method

--- p001hello.rb
+++ p001hello.rb
@@ -4,3 +4,8 @@ def say_goodnightRuby(name)
   "Good night, #{name}"
 end
 puts say_goodnightRuby("Daro, and thank you God!!")
+
+def praiseGod(praise)
+  "I will praise you " + praise
+end
+puts praiseGod("God!!!")
